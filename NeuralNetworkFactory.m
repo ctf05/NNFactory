77ba7e05654749a6872d2b234da943fcb03acc2b
Christian Beadling <christianbeadling@gmail.com>
2025-02-05 15:07:43 -0500

    idk

--- NeuralNetworkFactory.m
+++ NeuralNetworkFactory.m
@@ -3,25 +3,24 @@ function NeuralNetworkFactory
     allKeys = {};
     for i = 1:numel(imgs)
         [~, name, ~] = fileparts(imgs(i).name);
-        jsonStr = extractBetween(name, '{', '}', 'Boundaries','inclusive');
-        data = jsondecode(jsonStr{1});
+        jsonFile = fullfile('training', [name '.json']);
+        jsonStr = fileread(jsonFile);
+        data = jsondecode(jsonStr);
         allKeys = union(allKeys, fieldnames(data));
     end
     sortedKeys = sort(allKeys);
     numOutputs = numel(sortedKeys);
-    inputSize = [224 224 3];
+    inputSize = [224 224 1];
     images = zeros([inputSize, numel(imgs)], 'single');
     targets = zeros(numel(imgs), numOutputs);
     for i = 1:numel(imgs)
         img = imread(fullfile('training', imgs(i).name));
-        if size(img,3) == 1
-            img = repmat(img,1,1,3);
-        end
         img = imresize(img, inputSize(1:2));
         images(:,:,:,i) = single(img)/255;
         [~, name, ~] = fileparts(imgs(i).name);
-        jsonStr = extractBetween(name, '{', '}', 'Boundaries','inclusive');
-        data = jsondecode(jsonStr{1});
+        jsonFile = fullfile('training', [name '.json']);
+        jsonStr = fileread(jsonFile);
+        data = jsondecode(jsonStr);
         for j = 1:numOutputs
             targets(i,j) = data.(sortedKeys{j});
         end
@@ -51,10 +50,12 @@ function NeuralNetworkFactory
         regressionLayer
     ];
     options = trainingOptions('adam', ...
-        'MaxEpochs',30, ...
+        'MaxEpochs',5, ...
         'InitialLearnRate',1e-3, ...
         'ValidationData',{valImgs, valTargets}, ...
         'Plots','training-progress', ...
-        'Verbose',true);
+        'Verbose',true, ...
+        'ExecutionEnvironment','gpu', ...
+        'MiniBatchSize', 16);
     net = trainNetwork(trainImgs, trainTargets, layers, options);
 end
